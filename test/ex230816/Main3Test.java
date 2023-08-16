005282d656bd8db9a5858a7c97d0e8e7ba3ba6f1
chaerin <krin0525@naver.com>
2023-08-16 22:42:01 +0900

    feat: 숫자 문자열과 영단어

---
+++ test/ex230816/Main3Test.java
@@ -0,0 +1,14 @@
+package ex230816;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Main3Test {
+
+    @Test
+    public void solution() {
+        Main3 main = new Main3();
+        assertEquals(main.solution("one4seveneight"), 1478);
+    }
+}
