b19b1b072ed34a3056d7ffe0c29249976e9d2287
chaerin <krin0525@naver.com>
2023-09-05 22:44:16 +0900

    feat: n의 배수

---
+++ src/ex230905/Main4.java
@@ -0,0 +1,8 @@
+package ex230905;
+
+public class Main4 {
+    public int solution(int num, int n) {
+        if(num%n == 0) return 1;
+        else return 0;
+    }
+}
---
+++ test/ex230905/Main4Test.java
@@ -0,0 +1,14 @@
+package ex230905;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Main4Test {
+
+    @Test
+    public void solution() {
+        Main4 main = new Main4();
+        assertEquals(main.solution(98, 2), 1);
+    }
+}
